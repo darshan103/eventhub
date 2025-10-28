478c34e8f2cd9c147a54454fb46780e8dfc6d10f
darshan103 <singhdarshan2166@gmail.com>
2025-10-28 16:57:59 +0530

    deployed backend connection

--- src/app/eventservices/eventhub.service.ts
+++ src/app/eventservices/eventhub.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../enviroments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventhubService {
 
-  private apiUrl = 'http://localhost:1337/api/eventhub';
+  private apiUrl = environment.BASE_API_URL;
 
   constructor(private http: HttpClient) { }
 
---
+++ src/enviroments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+
+    BASE_API_URL: 'https://eventhubbackend-vwqr.onrender.com/api',
+    // BASE_API_URL: 'http://localhost:1337/api',
+};
