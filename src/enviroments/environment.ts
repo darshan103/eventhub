337d034a0e4f104b3bacd9d70227ab95dfb0e753
darshan103 <singhdarshan2166@gmail.com>
2025-12-23 09:47:42 +0530

    latest code updated

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
 
-    BASE_API_URL: 'https://eventhubbackend-vwqr.onrender.com/api',
+    BASE_API_URL: 'https://eventhubbackend-xi7u.onrender.com/api',
     // BASE_API_URL: 'http://localhost:1337/api',
 };
